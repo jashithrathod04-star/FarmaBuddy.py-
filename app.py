ff31b5a19ec559a5120246c14369ca9f0e455b9b
jashithrathod04-star <jashith.rathod04@gmail.com>
2026-01-29 22:46:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,13 @@ st.set_page_config(
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash-latest",
+    model_name="gemini-1.5-flash",
     generation_config={
         "temperature": 0.5,
         "max_output_tokens": 512
     }
 )
+
 
 
 
