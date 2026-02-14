59239eb7207d63f3cb693374ef0997dec11d4047
jashithrathod04-star <jashith.rathod04@gmail.com>
2026-02-15 01:46:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ st.markdown("""
 /* Make all standard text and labels bold and high-contrast */
 .stApp p, .stApp span, .stApp label {
     color: #d7fad8 !important; /* Deep dark green for better visibility */
-    font-weight: 800 !important;
+    font-weight: 600 !important;
     text-shadow: 1px 1px 2px rgba(255,255,255,0.8); /* White glow behind text */
 }
 
