9936c1c4406030f0c57b342115277570e64886f2
jashithrathod04-star <jashith.rathod04@gmail.com>
2026-02-15 00:31:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -219,6 +219,15 @@ st.markdown(f"""
     </div>
 </div>
 
+<style>
+@keyframes pulse {{
+0% {{transform: scale(1);}}
+50% {{transform: scale(1.05);}}
+100% {{transform: scale(1);}}
+}}
+
+</style>
+""", unsafe_allow_html=True)
 
 
 
